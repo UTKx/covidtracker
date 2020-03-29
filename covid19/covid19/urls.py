661c369ae5cba1613a55c3d8ad15808c68a0666f
Utkarsh <utkarshkumark@gmail.com>
2020-03-30 01:51:37 +0530

    added changes

--- covid19/covid19/urls.py
+++ covid19/covid19/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from tracker import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.track_all)
 ]
--- covid19/tracker/views.py
+++ covid19/tracker/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+import requests
+import json
+
 # Create your views here.
+
+def track_all(request):
+    url_all = "https://corona.lmao.ninja/all"
+    response = request.GET(url_all, verify=CERT_REQUIRED)
+    data = response.json()
+    print(data)
+    return render(request, 'index.html')
