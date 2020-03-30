1c43022850dc3443d7742a44cd5e2af813e18389
Utkarsh <utkarshkumark@gmail.com>
2020-03-30 13:55:58 +0530

    modified models,views and templates/index

--- covid19/tracker/models.py
+++ covid19/tracker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class covid_world(models.Model):
+    stat_id = models.AutoField(primary_key=True)
+    total_cases = models.IntegerField()
+    deaths = models.IntegerField()
+    recovered = models.IntegerField()
+
+    def __str__(self):
+        return self.world_stats
--- covid19/tracker/views.py
+++ covid19/tracker/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from django.core.serializers.json import DjangoJSONEncoder
+
+from .models import *
 
 import requests
 import json
+import ssl
+import re
+
 
 # Create your views here.
 
 def track_all(request):
     url_all = "https://corona.lmao.ninja/all"
-    response = request.GET(url_all, verify=CERT_REQUIRED)
+    response = requests.get(url_all, verify=True)
+    print(response)
     data = response.json()
-    print(data)
-    return render(request, 'index.html')
+    # print(data)
+    covid_world.objects.all().delete()
+    covid_world(
+        total_cases = data['cases'],
+        deaths = data['deaths'],
+        recovered = data['recovered']
+    ).save()
+    data = covid_world.objects.all().values('total_cases', 'deaths', 'recovered')
+    return render(request, 'index.html', {'queryset': data})
